efa6ca370de8eef223beaf08b4591a0a13622672
Biruk Tebebe <brook.diku1@gmail.com>
2023-07-27 20:41:46 +0300

    initial commit for Mengedegna

---
+++ src/main/java/com/mengedegan/Mengedegna/MengedegnaApplication.java
@@ -0,0 +1,13 @@
+package com.mengedegan.Mengedegna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MengedegnaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MengedegnaApplication.class, args);
+	}
+
+}
